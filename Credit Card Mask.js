c2ceef47a31334875d2adae21c6d9a2c83da594e
Max <133934232+Kleostro@users.noreply.github.com>
2023-08-27 13:49:02 +0300

    feat: credit Card Mask.js

---
+++ Credit Card Mask.js
@@ -0,0 +1,32 @@
+/*
+Credit Card Mask
+
+ESCRIPTION:
+
+  Usually when you buy something, you're asked whether your credit card number, phone number or answer to your most secret question is still correct. However, since someone could look over your shoulder, you don't want that shown on your screen. Instead, we mask it.
+  
+  Your task is to write a function maskify, which changes all but the last four characters into '#'.
+  
+  Examples (input --> output):
+  "4556364607935616" --> "############5616"
+       "64607935616" -->      "#######5616"
+                 "1" -->                "1"
+                  "" -->                 ""
+  
+  // "What was the name of your first pet?"
+  "Skippy" --> "##ippy"
+  "Nananananananananananananananana Batman!" --> "####################################man!"
+
+SOLUTION: 
+*/
+const maskify = (cc) => {
+  let result = ''
+  if(cc.length > 4) {
+    for(let i = 0; i < cc.length - 4; i++) {
+      result = result + '#'
+    }
+  } else if (cc.length < 4) {
+    return cc
+  }
+  return result + cc.slice(cc.length - 4)
+}
