307c6dce003250d3214c04706dc1b57b151c1bcd
Max <133934232+Kleostro@users.noreply.github.com>
2023-10-04 17:51:40 +0300

    feat: SillyCASE.js

---
+++ SillyCASE.js
@@ -0,0 +1,23 @@
+
+/*
+ SillyCASE
+
+ESCRIPTION:
+
+Create a function that takes a string and returns that string with the first half lowercased and the last half uppercased.
+
+eg: foobar == fooBAR
+
+If it is an odd number then 'round' it up to find which letters to uppercase. See example below.
+
+sillycase("brian")  
+//         --^-- midpoint  
+//         bri    first half (lower-cased)  
+//            AN second half (upper-cased)  
+
+SOLUTION: 
+*/
+function sillycase(silly) {
+    if (silly.length % 2 !== 0) return silly.slice(0, silly.length / 2 + 1).toLowerCase() + silly.slice(silly.length / 2 + 1).toUpperCase()
+    else return silly.slice(0, silly.length / 2).toLowerCase() + silly.slice(silly.length / 2).toUpperCase()
+  }
