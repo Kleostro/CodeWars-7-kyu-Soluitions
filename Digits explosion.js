836ee7827f795113c7a873dffd904915be034250
Max <133934232+Kleostro@users.noreply.github.com>
2023-10-07 15:48:48 +0300

    feat: Digits explosion.js

---
+++ Digits explosion.js
@@ -0,0 +1,22 @@
+/*
+Digits explosion
+
+ESCRIPTION:
+
+Given a string made of digits [0-9], return a string where each digit is repeated a number of times equals to its value.
+
+Examples
+"312" should return "333122"
+"102269" should return "12222666666999999999"
+
+SOLUTION: 
+*/
+
+const explode = (s) => {
+  let arr = [...s]
+  let result = ''
+  arr.forEach((letter) => {
+    letter === '0' ? result : result += letter.repeat(+letter)
+  })
+  return result;
+}
